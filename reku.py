a9a6f4fbbab6171a9484b9f932bfdd79753a39c1
Jacek Grzechnik <jacek.grzechnik@gmail.com>
2017-12-30 20:42:11 +0100

    Another condition for starting buffer filling

--- reku.py
+++ reku.py
@@ -6,7 +6,7 @@ import time
 
 ser = serial.Serial()
 ser.baudrate = 57600
-ser.timeout=3
+ser.timeout = 3
 
 #ser.inWaiting - bytes to read?
 
@@ -16,19 +16,19 @@ ser.port = '/dev/cuaU0'
 # http://stackoverflow.com/questions/19317766/how-to-find-first-byte-of-a-serial-stream-with-python
 # czytac do bufora a potem dalej rozbijac na linie
 try:
-    x=''
-    cnt=0
+    x = ''
+    cnt = 0
     ser.open()
     if ser.is_open:
         #while x != '<' or x != '('
         while cnt<3:
             x = ser.read(1)
             print 'x=' + x
-            if x== '<' or x == '(':
-                y=ser.read(117)
+            if x == '<' or x == '(' or x == '>' or x == 'P':
+                y = ser.read(117)
                 cnt = cnt+1
          #   time.sleep(0.2)
-        z=ser.read(12)
+        z = ser.read(12)
         while True: 
             y = ser.read(117) 
             print 'y = ' + y
