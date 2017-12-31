1ef16a9968bd8806882fe4414dc6ae734254bfa7
Jacek Grzechnik <jacek.grzechnik@gmail.com>
2017-12-31 21:30:26 +0100

    process_line method, Reading class draft

---
+++ reading.py
@@ -0,0 +1,14 @@
+
+class Reading(object):
+
+    def __init__(self):
+        self.curr_time = None
+        self.curr_date = None
+        self.curr_flow = None
+        self.program_until = None
+        self.displayed_temp = None
+        self.curr_mode = None
+
+        self.inlet_temp = None
+        self.outlet_temp = None
+        self.external_temp = None
--- reku.py
+++ reku.py
@@ -1,37 +1,72 @@
-import serial
 import time
+import serial
+from reading import Reading
 
-# linia ma 117 bajtow, 
-# sleep 0.2 miedzy odczytami
+# line has 117 bytes,
+# payload has 80 bytes
+# need to sleep 0.2 between readings
 
 ser = serial.Serial()
 ser.baudrate = 57600
 ser.timeout = 3
 
-#ser.inWaiting - bytes to read?
 
 ser.parity = serial.PARITY_EVEN
+
+# freebsd:
 ser.port = '/dev/cuaU0'
-#ser.port = '/dev/ttyU0'
+
+# linux:
+# ser.port = '/dev/ttyU0'
+
 # http://stackoverflow.com/questions/19317766/how-to-find-first-byte-of-a-serial-stream-with-python
 # czytac do bufora a potem dalej rozbijac na linie
+
+def process_line(line=None):
+    if not line:
+        return
+
+    print('"{}"'.format(line))
+    curr_time = line[0:8]
+    curr_date = line[8:20].strip()
+    curr_flow_raw = line[20:30].strip()
+    curr_flow = curr_flow_raw.split('Wenty.=')[1]
+    program_until = line[30:40].strip()
+    displayed_temp = line[40:60].strip()
+    curr_mode = line[60:80].strip()
+
+    print('time:"{}"\ndate:"{}"\nflow_raw:"{}"\nflow:"{}"\nuntil:"{}"\ntemp:"{}"\nmode:"{}"'
+          ''.format(curr_time,
+                    curr_date, 
+                    curr_flow_raw,
+                    curr_flow,
+                    program_until,
+                    displayed_temp,
+                    curr_mode))
+
 try:
     x = ''
     cnt = 0
     ser.open()
     if ser.is_open:
-        #while x != '<' or x != '('
-        while cnt<3:
+
+        # prepare buffer
+        while cnt < 3:
             x = ser.read(1)
-            print 'x=' + x
+            # print 'x=' + x
             if x == '<' or x == '(' or x == '>' or x == 'P':
                 y = ser.read(117)
                 cnt = cnt+1
-         #   time.sleep(0.2)
         z = ser.read(12)
-        while True: 
-            y = ser.read(117) 
-            print 'y = ' + y
+
+        # read actual lines
+        while True:
+            y = ser.read(117)
+            print y
+            line_stripped = y[5:-32]
+            if len(line_stripped) < 80:
+                continue
+            process_line(line=line_stripped)
             time.sleep(0.2)
 
 #        while True:
@@ -40,12 +75,19 @@ try:
 #            time.sleep(0.2)
 
 except Exception as ex:
-	print ex.message
-	ser.close()
-except:
-    print 'Port is closed'
+    print('{}'.format(ex.message))
     ser.close()
 
-if ser.is_open:
-    print 'Port is open - closing'
+except KeyboardInterrupt:
+    print('Interrupt received, stopping')
+
+finally:
+    print('closing port')
     ser.close()
+    print('Port is closed')
+
+
+if ser.is_open:
+    print('Port is still open - closing')
+    ser.close()
+
