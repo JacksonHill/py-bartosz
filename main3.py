12fcbc236bc725d27aef8cb5f7829ec1b8198418
Jacek Grzechnik <jacek.grzechnik@gmail.com>
2023-12-31 14:36:56 +0100

    py3 compatibility

--- main3.py
+++ main3.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import time
 import serial
 from reading import Reading
@@ -26,7 +24,7 @@ def process_line(line=None):
     if not line:
         return None
 
-    # print("'{}'".format(line))
+    #print("'{}'".format(line))
     raw_reading = {
         'curr_time': line[0:8],
         'curr_date': line[8:20].strip(),
@@ -49,8 +47,9 @@ try:
         # prepare buffer
         while cnt < 3:
             x = ser.read(1)
-            # print ('x={}'.format(x))
-            if x == '<' or x == '(' or x == '>' or x == 'P':
+            #print (f'prep: {x}')
+            #if x == b'<' or x == b'(' or x == b'>' or x == b'P':
+            if x in (b'<', b'(', b'>', b'P'):
                 y = ser.read(117)
                 cnt = cnt+1
         z = ser.read(12)
@@ -58,8 +57,11 @@ try:
         # read actual lines
         while True:
             y = ser.read(117)
-            # print(y)
+            #print(y)
             line_stripped = y[5:-32]
+            #print (f"line stripped: \n{line_stripped}")
+            line_stripped = line_stripped.decode('iso8859-2')
+
             if len(line_stripped) < 80:
                 continue
             raw = process_line(line=line_stripped)
